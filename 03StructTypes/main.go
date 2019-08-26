b23d6bb0845fa5441dba3363e4d90feb010d4bff
msisiliani <marcela.silva@resultadosdigitais.com.br>
2019-08-26 19:09:15 -0300

    Add 03-Struct-Types exercises

--- 03StructTypes/main.go
+++ 03StructTypes/main.go
@@ -7,17 +7,45 @@
 // Declare and initialize an anonymous struct type with the same three fields. Display the value.
 package main
 
+import "fmt"
+
 // Add imports.
 
 // Add user type and provide comment.
+type user struct {
+	name  string
+	email string
+	age   int8
+}
 
 func main() {
 
 	// Declare variable of type user and init using a struct literal.
+	userL := user{
+		name:  "Literal Variable",
+		email: "literal@foo.com",
+		age:   31,
+	}
 
 	// Display the field values.
+	fmt.Println("Name:", userL.name)
+	fmt.Println("Email:", userL.email)
+	fmt.Println("Age:", userL.age)
 
 	// Declare a variable using an anonymous struct.
 
+	userA := struct {
+		name  string
+		email string
+		age   int8
+	}{
+		name:  "Anonymous Variable",
+		email: "anonymous@foo.com",
+		age:   31,
+	}
+
 	// Display the field values.
+	fmt.Println("Name:", userA.name)
+	fmt.Println("Email:", userA.email)
+	fmt.Println("Age:", userA.age)
 }
